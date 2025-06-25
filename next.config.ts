f3830a04f60e96520db83de3934c7525b19fb775
shanekeen-real <shane15076541@gmail.com>
2025-06-25 05:30:26 +0100

    rename test config files to fix build

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,30 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.test.ts': {
+          loaders: ['ignore-loader'],
+        },
+        '*.test.tsx': {
+          loaders: ['ignore-loader'],
+        },
+        '*.spec.ts': {
+          loaders: ['ignore-loader'],
+        },
+        '*.spec.tsx': {
+          loaders: ['ignore-loader'],
+        },
+        'vitest.config.ts': {
+          loaders: ['ignore-loader'],
+        },
+        'playwright.config.ts': {
+          loaders: ['ignore-loader'],
+        },
+      },
+    },
+  },
   /* config options here */
 };
 
